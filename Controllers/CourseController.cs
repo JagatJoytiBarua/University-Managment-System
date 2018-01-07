fd975f575bdda62f23766b17162c0b3e3054c87f
Jagat Joyti Barua <jagat99@gmail.com>
2018-01-08 01:40:55 +0600

    Add files via upload

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityManagementSystemByDigiminds.Manager;
+using UniversityManagementSystemByDigiminds.Models;
+
+namespace UniversityManagementSystemByDigiminds.Controllers
+{
+    public class CourseController : Controller
+    {
+
+        DepartmentManager departmentManager = new DepartmentManager();
+        SemesterManager semesterManager = new SemesterManager();
+        CourseManager courseManager = new CourseManager();
+
+
+        public ActionResult Save()
+        {
+
+
+            List<Department> departments = departmentManager.GetAll();
+            List<Semester> semesters = semesterManager.GetAll();
+            ViewBag.Departments = departments;
+            ViewBag.Semesters = semesters;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Course aCourse)
+        {
+            string msg = courseManager.Save(aCourse);
+            ViewBag.message = msg;
+            List<Department> departments = departmentManager.GetAll();
+            List<Semester> semesters = semesterManager.GetAll();
+            ViewBag.Departments = departments;
+            ViewBag.Semesters = semesters;
+
+            ModelState.Clear();
+
+            return View();
+        }
+    }
+}
